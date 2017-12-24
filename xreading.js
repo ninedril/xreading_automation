54e13abceb50aeaf7d536f8faea36db62c250a3e
ninedril@t90chi <ninedril-t90chi@gmail.com>
2017-12-25 00:53:08 +0900

    M main(is_first)

--- xreading.js
+++ xreading.js
@@ -1,49 +1,57 @@
-function main() {
+function main(is_first = false) {
 	var nbt = document.evaluate('//div[contains(text(), "Next")]', document, null).iterateNext();
 	if (!nbt) {
 		console.log('Something is wrong');
 		return;
 	}
 	if (nbt.style.display != 'none') {
-		nbt.click();
-		//1ページ読むのにかかる時間を指定
-		var rand = Math.random()*60 + 40;
+		if (!is_first) {
+			nbt.click();
+		}
+		
 		//main process
 		checkDialog();
-		getText();
+		var current_text = getText();
 
+		//1ページ読むのにかかる時間を指定 25字（含スペース）/秒 画像1枚/15秒
+		var img_count = document.querySelectorAll('div#xContent img').length;
+		var rand = (current_text.length/25 + (img_count+1)*10) * (Math.random()*0.2 + 0.9);
 		setTimeout(main, rand*1000);
 	} else {
-		var cbt = document.evaluate('//div[contains(text(), "Close")]', document, null).iterateNext();
-		if(cbt){cbt.click();}
+		//var cbt = document.evaluate('//div[contains(text(), "Close")]', document, null).iterateNext();
+		//if(cbt){cbt.click();}
 	}
 }
-function doScroll(pm) {
-	if (pm == 'down') {
+
+function startScroll(first_direction) {
+	if (first_direction == 'down') {
 		//下スクロール。どの距離をスクロールするか指定
 		var r = (Math.random()*0.2 + 0.8) * document.body.clientHeight;
 		scrollBy(0, r);
 		//何秒に一回スクロールするか指定
-		setTimeout(function(){doScroll('up');}, 15*1000);
+		setTimeout(function(){startScroll('up');}, 15*1000);
 	} else {
 		//上スクロール。どの距離をスクロールするか指定
 		var r = (Math.random()*0.8 + 0.2) * document.body.clientHeight;
 		scrollBy(0, -r);
 		//何秒に一回スクロールするか指定
-		setTimeout(function(){doScroll('down');}, 8*1000);
+		setTimeout(function(){startScroll('down');}, 8*1000);
 	}
 }
+
 function checkDialog() {
 	var continue_bt = document.evaluate('//button[descendant::text()[contains(., "Continue")]]', document, null).iterateNext();
 	if(continue_bt) {
 		continue_bt.click();
 	}
 }
+
 function getText() {
 	var new_text = document.getElementById('xContent').innerText.replace(/^\s*|\s*$/g, '');
 	whole_text = whole_text + new_text;
+	return new_text;
 }
+
 whole_text = '';
-getText();
-doScroll('down');
-setTimeout(main, 10*1000);
+startScroll('down');
+main(true)
